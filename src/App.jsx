1c0bff15ae6ed8c3c8334704b0681390481adfff
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-22 20:23:28 +0700

    feat: Remove default app

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
-import React, { useState } from 'react'
-
-import ReactLogo from '@/Assets/React.svg'
-import ViteLogo from '@/Assets/Vite.svg'
-
-import '@/App.css'
+import React from 'react'
 
 const App = () => {
-	const [Count, SetCount] = useState(0)
-
-	return (
-		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-					<img src={ViteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-					<img
-						src={ReactLogo}
-						className='logo react'
-						alt='React logo'
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => SetCount(count => count + 1)}>
-					count is {Count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
