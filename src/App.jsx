c97107019e1181fc8faa6bfd1461bee16945d3cf
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-22 17:45:59 +0700

    feat: Entry

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+import ReactLogo from './assets/React.svg'
+import ViteLogo from './assets/Vite.svg'
+
+import './App.css'
+
+const App = () => {
+	const [Count, SetCount] = useState(0)
+
+	return (
+		<div className='App'>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src={ViteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://reactjs.org' target='_blank'>
+					<img
+						src={ReactLogo}
+						className='logo react'
+						alt='React logo'
+					/>
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button onClick={() => SetCount(count => count + 1)}>
+					count is {Count}
+				</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>
+				Click on the Vite and React logos to learn more
+			</p>
+		</div>
+	)
+}
+
+export default App
