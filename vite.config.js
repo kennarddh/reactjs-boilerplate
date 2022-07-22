7a2db91bcc149129d43c5ed1091a9dc110032e93
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-22 19:42:25 +0700

    chore: Eslint vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 
+import eslintPlugin from 'vite-plugin-eslint'
+
 import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
@@ -9,7 +11,7 @@ export default ({ mode }) => {
 	const { PORT } = { ...loadEnv(mode, process.cwd(), '') }
 
 	return defineConfig({
-		plugins: [react()],
+		plugins: [react(), eslintPlugin()],
 		resolve: {
 			alias: {
 				'@': resolve(__dirname, './src'),
