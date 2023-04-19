6df3d5847e78cd45f5483f651202c8596791fedc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-04-20 00:55:58 +0700

    feat: Remove all test and useless component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { FC } from 'react'
 
-import Button from 'Components/Button'
-
 const App: FC = () => {
-	return (
-		<div>
-			<div data-testid='custom-element'>heading</div>
-			<Button>Blue Button</Button>
-			<Button red>Red Button</Button>
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
--- src/Components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-import { Props } from './Types'
-
-const Button = styled.button<Partial<Props>>`
-	color: ${({ red }) => (red ? '#ff0000' : '#0000ff')};
-`
-
-export default Button
