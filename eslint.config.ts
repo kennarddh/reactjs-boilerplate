ae1b85178d07b6f52c40e949f83e83db560a039a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-01-18 02:34:51 +0700

    chore: Remove deprecated eslint rules and groups specific rules
    Sync with other projects

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,63 +1,90 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import js from '@eslint/js'
-import eslintPluginImportX from 'eslint-plugin-import-x'
+import importX from 'eslint-plugin-import-x'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import prettier from 'eslint-plugin-prettier'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
-import reactPlugin from 'eslint-plugin-react'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
 import * as tsEslint from 'typescript-eslint'
 
 export default tsEslint.config(
-	jsxA11y.flatConfigs.recommended,
-	prettierRecommended,
+	js.configs.recommended,
+	importX.flatConfigs.recommended,
+	importX.flatConfigs.typescript,
 	tsEslint.configs.strictTypeChecked,
 	tsEslint.configs.stylisticTypeChecked,
-	js.configs.recommended,
-	reactPlugin.configs.flat.recommended!,
-	reactPlugin.configs.flat['jsx-runtime']!,
-	eslintPluginImportX.flatConfigs.recommended,
-	eslintPluginImportX.flatConfigs.typescript,
-	reactHooks.configs['recommended-latest'],
-	{ ignores: ['build/**/*'] },
+	prettierRecommended,
+	{ name: 'Ignores', ignores: ['build/**/*'] },
 	{
+		name: 'Main',
+		files: ['**/*.{ts,tsx}', 'eslint.config.ts'],
+		...react.configs.flat.recommended!,
+		...react.configs.flat['jsx-runtime']!,
+		...reactHooks.configs['recommended-latest'],
+		...jsxA11y.flatConfigs.recommended,
 		languageOptions: {
+			...react.configs.flat.recommended!.languageOptions,
+			...react.configs.flat['jsx-runtime']!.languageOptions,
+			...jsxA11y.flatConfigs.recommended.languageOptions,
 			parser: tsEslint.parser,
 			parserOptions: {
 				ecmaFeatures: {
 					jsx: true,
 				},
-				ecmaVersion: 'latest',
-				sourceType: 'module',
+				jsxPragma: null,
+				ecmaVersion: 2025,
 				tsconfigRootDir: import.meta.dirname,
 				projectService: true,
 			},
 			globals: {
 				...globals.browser,
-				...globals.es2021,
+				...globals.es2025,
 			},
 		},
 		plugins: {
-			react: reactPlugin,
+			react,
+			'react-hooks': reactHooks,
+			'jsx-a11y': jsxA11y,
 			prettier,
 			'@typescript-eslint': tsEslint.plugin,
 		},
 		rules: {
+			...react.configs.flat.recommended!.rules,
+			...react.configs.flat['jsx-runtime']!.rules,
+			...reactHooks.configs['recommended-latest'].rules,
+			...jsxA11y.flatConfigs.recommended.rules,
 			'no-unused-vars': 'off',
-			'@typescript-eslint/no-unused-vars': 'warn',
-			'prettier/prettier': [
+			'@typescript-eslint/no-unused-vars': [
 				'warn',
 				{
-					endOfLine: 'lf',
+					argsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
 				},
 			],
-			'import-x/prefer-default-export': 'off',
-			'linebreak-style': ['error', 'unix'],
-			quotes: ['error', 'single'],
-			semi: ['error', 'never'],
-			'react/prop-types': 'off',
-			'no-case-declarations': 'off',
+			'@typescript-eslint/no-empty-function': [
+				'error',
+				{ allow: ['private-constructors', 'protected-constructors'] },
+			],
+			'@typescript-eslint/no-misused-promises': [
+				'error',
+				{
+					checksVoidReturn: false,
+				},
+			],
+			'@typescript-eslint/restrict-template-expressions': [
+				'error',
+				{
+					allowNumber: true,
+				},
+			],
+			'@typescript-eslint/require-await': 'off',
+			'@typescript-eslint/no-confusing-void-expression': 'off',
+			'import-x/extensions': ['warn', { ts: 'never', json: 'never' }],
+			'import-x/no-named-as-default-member': 'off',
 			'react/jsx-curly-brace-presence': 'warn',
 			'react/jsx-no-useless-fragment': 'warn',
 			'import-x/no-unresolved': [
@@ -67,17 +94,23 @@ export default tsEslint.config(
 				},
 			],
 			'react/jsx-filename-extension': [
-				1,
+				'warn',
 				{ extensions: ['.ts', '.tsx'] },
 			],
-			'react/react-in-jsx-scope': 'off',
-			'import-x/extensions': ['warn', { ts: 'never', json: 'never' }],
-			'import-x/no-named-as-default-member': 'off',
 		},
 		settings: {
 			react: {
 				version: 'detect',
 			},
+		},
+	},
+	{
+		name: 'Prettier Warn',
+		plugins: {
+			prettier,
+		},
+		rules: {
+			'prettier/prettier': 'warn',
 		},
 	},
 )
