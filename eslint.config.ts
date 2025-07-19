ddab7ae2f0790ea582271057df63f63deca801d1
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-07-19 22:54:48 +0700

    chore: Updates

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import js from '@eslint/js'
-import importX from 'eslint-plugin-import-x'
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript'
+import { importX } from 'eslint-plugin-import-x'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import prettier from 'eslint-plugin-prettier'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
+import * as reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
 import * as tsEslint from 'typescript-eslint'
 
@@ -20,41 +20,21 @@ export default tsEslint.config(
 	{
 		name: 'Main',
 		files: ['**/*.{ts,tsx}', 'eslint.config.ts'],
-		...react.configs.flat.recommended!,
-		...react.configs.flat['jsx-runtime']!,
-		...reactHooks.configs['recommended-latest'],
-		...jsxA11y.flatConfigs.recommended,
 		languageOptions: {
-			...react.configs.flat.recommended!.languageOptions,
-			...react.configs.flat['jsx-runtime']!.languageOptions,
-			...jsxA11y.flatConfigs.recommended.languageOptions,
 			parser: tsEslint.parser,
 			parserOptions: {
-				ecmaFeatures: {
-					jsx: true,
-				},
-				jsxPragma: null,
 				ecmaVersion: 2025,
 				tsconfigRootDir: import.meta.dirname,
 				projectService: true,
 			},
 			globals: {
-				...globals.browser,
 				...globals.es2025,
 			},
 		},
 		plugins: {
-			react,
-			'react-hooks': reactHooks,
-			'jsx-a11y': jsxA11y,
-			prettier,
 			'@typescript-eslint': tsEslint.plugin,
 		},
 		rules: {
-			...react.configs.flat.recommended!.rules,
-			...react.configs.flat['jsx-runtime']!.rules,
-			...reactHooks.configs['recommended-latest'].rules,
-			...jsxA11y.flatConfigs.recommended.rules,
 			'no-unused-vars': 'off',
 			'@typescript-eslint/no-unused-vars': [
 				'warn',
@@ -85,14 +65,46 @@ export default tsEslint.config(
 			'@typescript-eslint/no-confusing-void-expression': 'off',
 			'import-x/extensions': ['warn', { ts: 'never', json: 'never' }],
 			'import-x/no-named-as-default-member': 'off',
-			'react/jsx-curly-brace-presence': 'warn',
-			'react/jsx-no-useless-fragment': 'warn',
+		},
+	},
+	{
+		name: 'Frontend',
+		files: ['packages/frontend/**/*.{ts,tsx}'],
+		...react.configs.flat.recommended,
+		...react.configs.flat['jsx-runtime'],
+		...jsxA11y.flatConfigs.recommended,
+		languageOptions: {
+			...react.configs.flat.recommended?.languageOptions,
+			...react.configs.flat['jsx-runtime']?.languageOptions,
+			...jsxA11y.flatConfigs.recommended.languageOptions,
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+				jsxPragma: null,
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'jsx-a11y': jsxA11y,
+		},
+		rules: {
+			...react.configs.flat.recommended?.rules,
+			...react.configs.flat['jsx-runtime']?.rules,
+			...reactHooks.configs['recommended-latest'].rules,
+			...jsxA11y.flatConfigs.recommended.rules,
 			'import-x/no-unresolved': [
 				'error',
 				{
 					ignore: ['^virtual:'],
 				},
 			],
+			'react/jsx-curly-brace-presence': 'warn',
+			'react/jsx-no-useless-fragment': 'warn',
 			'react/jsx-filename-extension': [
 				'warn',
 				{ extensions: ['.ts', '.tsx'] },
@@ -102,6 +114,12 @@ export default tsEslint.config(
 			react: {
 				version: 'detect',
 			},
+			'import-x/resolver-next': [
+				createTypeScriptImportResolver({
+					alwaysTryTypes: true,
+					project: 'packages/frontend/tsconfig.json',
+				}),
+			],
 		},
 	},
 	{
